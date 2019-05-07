0751563986b5ca2fef87fd32503b786b65145ac8
carlos.lopeza@tecnocom.biz <Tecnocom2018>
2019-05-07 15:09:57 -0500

    .ViewPager added in activity_main. .FirstFragment -> newInstance, onCreate, onCreateView

---
+++ app/src/main/java/com/eduardo/viewpagerfragments/Fragments/FirstFragment.java
@@ -0,0 +1,53 @@
+package com.eduardo.viewpagerfragments.Fragments;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.eduardo.viewpagerfragments.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FirstFragment extends Fragment {
+
+    private String title;
+    private int page;
+
+    public FirstFragment() {
+        // Required empty public constructor
+    }
+
+    public static FirstFragment newInstance(int page, String title) {
+        FirstFragment fragment = new FirstFragment();
+        Bundle args = new Bundle();
+        args.putInt("someInt", page);
+        args.putString("someTitle", title);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        title = getArguments().getString("someInt");
+        page = getArguments().getInt("someInt");
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        /*return inflater.inflate(R.layout.fragment_first, container, false);*/
+        View view =  inflater.inflate(R.layout.fragment_first, container, false);
+        TextView tvLabel = view.findViewById(R.id.tvLabel);
+        tvLabel.setText(page+"---"+title);
+        return view;
+    }
+
+}
