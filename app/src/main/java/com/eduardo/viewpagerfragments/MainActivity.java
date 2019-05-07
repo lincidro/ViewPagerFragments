d7b70bef3e737c830ef106f486f8aa710aae5bd8
carlos.lopeza@tecnocom.biz <Tecnocom2018>
2019-05-07 15:36:14 -0500

    Associate the ViewPager with a new instance of our adapter in "onCreate" Method in MainActivity.

--- app/src/main/java/com/eduardo/viewpagerfragments/MainActivity.java
+++ app/src/main/java/com/eduardo/viewpagerfragments/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -11,10 +12,17 @@ import com.eduardo.viewpagerfragments.Fragments.FirstFragment;
 
 public class MainActivity extends AppCompatActivity {
 
+    FragmentPagerAdapter fragmentPagerAdapter;
+    ViewPager viewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        viewPager = findViewById(R.id.vpPager);
+        fragmentPagerAdapter = new MyPageAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(fragmentPagerAdapter);
+
     }
 
     public static class MyPageAdapter extends FragmentPagerAdapter{
@@ -44,10 +52,9 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-        @Nullable
         @Override
         public CharSequence getPageTitle(int position) {
-            return "PaGe: "+super.getPageTitle(position);
+            return "Página: "+super.getPageTitle(position);
         }
     }
 }
